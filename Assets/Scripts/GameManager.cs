fa0c7e1f1151ca73e764ad3f9b4639d13280557a
M.Plastunov <plohoyprogrammist@gmail.com>
2025-04-22 13:25:44 +0700

    Сделали короутину

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
-public List<GameObject> targets;
- 
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
 
+    private void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
