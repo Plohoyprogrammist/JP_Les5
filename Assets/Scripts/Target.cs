7ae0251015ce97deb98b02a68bff9a74c47467ab
M.Plastunov <plohoyprogrammist@gmail.com>
2025-04-22 12:02:16 +0700

    Создали код!

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Rigidbody targetRb;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
     }
 }
