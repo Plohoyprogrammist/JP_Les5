070006cf3839df8e38caa7eb6b0d4c69080b519a
M.Plastunov <plohoyprogrammist@gmail.com>
2025-04-23 20:11:38 +0700

    Со временем обновление счёта

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,12 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
+        UpdateScore(0);
+    }
+
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
         scoreText.text = "Score: " + score;
     }
 
@@ -24,6 +30,7 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
     }
 }
